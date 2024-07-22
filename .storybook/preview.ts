9b1932b7f7583fad8103f260ada76510bb0bb0d1
artFetisov <fetishfestoff@mail.ru>
2024-07-22 15:49:48 +0300

    fix:  add dark theme for storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,12 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react'
 import '../src/styles/index.scss'
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [{ name: 'dark', value: '#000' }],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
@@ -10,6 +14,6 @@ const preview: Preview = {
       },
     },
   },
-};
+}
 
-export default preview;
+export default preview
