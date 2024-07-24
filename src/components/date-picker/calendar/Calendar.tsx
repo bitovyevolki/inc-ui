64e269751c90871f1ba09d8f48db2cfdefe37ed3
artFetisov <fetishfestoff@mail.ru>
2024-07-24 14:54:36 +0300

    ref: add picker

--- src/components/date-picker/calendar/Calendar.tsx
+++ src/components/date-picker/calendar/Calendar.tsx
@@ -34,31 +34,3 @@ function Calendar({ showOutsideDays = true, ...props }: CalendarProps) {
 Calendar.displayName = 'Calendar'
 
 export { Calendar }
-
-export const LeftArrow = () => {
-  return (
-    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="36" height="36" rx="18" fill="#4C4C4C" />
-      <mask id="mask0_74201_991" maskUnits="userSpaceOnUse" x="8" y="8" width="20" height="20">
-        <rect x="8" y="8" width="20" height="20" fill="#D9D9D9" />
-      </mask>
-      <g mask="url(#mask0_74201_991)">
-        <path d="M20 23L15 18L20 13L21.062 14.062L17.125 18L21.062 21.938L20 23Z" fill="white" />
-      </g>
-    </svg>
-  )
-}
-
-export const RightArrow = () => {
-  return (
-    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="36" height="36" rx="18" fill="#4C4C4C" />
-      <mask id="mask0_74201_996" maskUnits="userSpaceOnUse" x="8" y="8" width="20" height="20">
-        <rect x="8" y="8" width="20" height="20" fill="#D9D9D9" />
-      </mask>
-      <g mask="url(#mask0_74201_996)">
-        <path d="M16 23L14.938 21.938L18.875 18L14.938 14.062L16 13L21 18L16 23Z" fill="white" />
-      </g>
-    </svg>
-  )
-}
