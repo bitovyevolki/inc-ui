e565d0fd2b241aed26803410ffb07354e0dc6c79
VladimirS <v.a.samarin@yandex.ru>
2024-07-31 13:08:40 +0300

    refactor dropdown

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -3,16 +3,15 @@ import type { ReactNode } from 'react'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import s from './Dropdown.module.scss'
-
 import { Typography } from '../typography'
 
-type DropdownType = {
+type DropdownProps = {
   children: ReactNode
   title?: string
   trigger: ReactNode
-}
+} & DropdownMenu.DropdownMenuProps
 
-export const Dropdown = ({ children, title, trigger }: DropdownType) => {
+export const Dropdown = ({ children, title, trigger, ...rest }: DropdownProps) => {
   return (
     <div className={s.wrapper}>
       <Typography variant={'subTitle2'}>
@@ -22,7 +21,7 @@ export const Dropdown = ({ children, title, trigger }: DropdownType) => {
           </DropdownMenu.Trigger>
 
           <DropdownMenu.Portal>
-            <DropdownMenu.Content align={'end'} className={s.content} sideOffset={4}>
+            <DropdownMenu.Content align={'end'} className={s.content} sideOffset={4} {...rest}>
               {title && (
                 <>
                   <p className={s.title}>{title}</p>
@@ -39,17 +38,17 @@ export const Dropdown = ({ children, title, trigger }: DropdownType) => {
   )
 }
 
-type DropdownItemType = {
+type DropdownItemProps = {
   arrow?: boolean
   icon: ReactNode
   title: string
-}
+} & DropdownMenu.DropdownMenuItemProps
 
-export const DropdownItem = ({ arrow = true, icon, title }: DropdownItemType) => {
+export const DropdownItem = ({ arrow = true, icon, title, ...rest }: DropdownItemProps) => {
   return (
     <Typography variant={'caption'}>
       {arrow && <DropdownMenu.Separator className={s.separator} />}
-      <DropdownMenu.Item className={s.item}>
+      <DropdownMenu.Item className={s.item} {...rest}>
         {icon}
         <p>{title}</p>
       </DropdownMenu.Item>
