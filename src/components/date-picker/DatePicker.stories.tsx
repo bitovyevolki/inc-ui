ba4683f2206d941491bea23ceb5fb689a4cadbc8
artFetisov <fetishfestoff@mail.ru>
2024-08-14 14:46:04 +0300

    date-picker ref: change stories

--- src/components/date-picker/DatePicker.stories.tsx
+++ src/components/date-picker/DatePicker.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { DatePicker } from './DatePicker'
+import { useState } from 'react'
 
 const meta: Meta<typeof DatePicker> = {
   component: DatePicker,
@@ -18,27 +19,32 @@ const meta: Meta<typeof DatePicker> = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Base: Story = {
-  args: {
-    label: 'Date of birth',
-  },
+export const BaseExample = () => {
+const [value, onChange] = useState<Date>()
+
+   return <DatePicker value={value} onChange={onChange} label='Date of birth'/>
 }
 
-export const WithError: Story = {
-  args: {
-    errorMessage: 'Incorrect date',
-    label: 'Date of birth',
-  },
+export const WithErrorExample = () => {
+   const [value, onChange] = useState<Date>()
+
+   return <DatePicker  errorMessage={'Incorrect date'}  value={value} onChange={onChange} label='Date of birth'/>
 }
 
-export const WithInitValue: Story = {
-  args: {
-    label: 'Date of birth',
-    value: new Date(),
-  },
+export const WithErrorLinkExample = () => {
+  const [value, onChange] = useState<Date>()
+
+   return <DatePicker errorMessage={'Incorrect date'}  Link={<a href='/' style={{marginLeft: '4px'}}>Privacy policy</a>}   value={value} onChange={onChange} label='Date of birth'/>
+ 
 }
 
-export const Disabled: Story = {
+export const WithInitValueExample = () => {
+  const [value, onChange] = useState<Date | undefined>(new Date())
+
+  return <DatePicker value={value} onChange={onChange} label='Date of birth'/>
+}
+
+export const DisabledExample: Story = {
   args: {
     disabled: true,
     label: 'Date of birth',
